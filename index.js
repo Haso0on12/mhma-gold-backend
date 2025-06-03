c9f54e60647468a8ed5ac656bcaea391725c21fe
Haso0on12 <haso0on12@hotmail.com>
2025-06-03 09:42:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,11 @@ app.use(cors());
 app.get('/api/gold-prices', (req, res) => {
 res.json({
 {
-  "timestamp": 1748932281,
-  "metal": "XAU",
-  "currency": "SAR",
-  "exchange": "GOLDAPI",
-  "symbol": "GOLDAPI:XAUSAR",
-  "open_time": 1748908800,
-  "ask": 12617.0887,
-  "bid": 12613.0751,
-  "price": 12615.2132,
-  "ch": -69.281,
-  "price_gram_24k": 405.5885,
-  "price_gram_22k": 371.7895,
-  "price_gram_21k": 354.89,
-  "price_gram_20k": 337.9904,
-  "price_gram_18k": 304.1914,
-}
+ res.json({
+    "24K": { sell: "280", buy: "285" },
+    "22K": { sell: "260", buy: "265" },
+    "21K": { sell: "250", buy: "255" },
+    "18K": { sell: "220", buy: "225" }
 });
 
 app.listen(PORT, () => {
